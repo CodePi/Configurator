d1c071d3ba03b6e592625ab4e60ad9180df8bd16
pilardi <CodePi@users.noreply.github.com>
2016-02-04 22:34:39 -0500

    removed email address

--- Configurator/configurator.cpp
+++ Configurator/configurator.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2011 Paul Ilardi (configurator@pilardi.com)
+// Copyright (C) 2011 Paul Ilardi (http://github.com/CodePi)
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a 
 // copy of this software and associated documentation files (the "Software"),
