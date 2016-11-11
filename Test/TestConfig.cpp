0429b46fadbc1d232c3bb1310dadea4aaad648e0
Paul Ilardi <root@vmweb2>
2016-11-10 22:35:22 -0500

    replaced tabs with spaces

--- Test/TestConfig.cpp
+++ Test/TestConfig.cpp
@@ -5,58 +5,58 @@ using namespace std;
 using namespace codepi;
 
 int main(){
-	TestConfig tc, tc2, tc3;
+  TestConfig tc, tc2, tc3;
 
-	try{
-		tc.readString("s.i=999");
+  try{
+    tc.readString("s.i=999");
 
-		tc.opt1 = 1;
+    tc.opt1 = 1;
 
-		tc.jjj=6;
+    tc.jjj=6;
 
-		tc.set("jjj","8");
+    tc.set("jjj","8");
 
-		tc.readString("jjj=12");
-		tc.readString("jjj=0xBAD");
+    tc.readString("jjj=12");
+    tc.readString("jjj=0xBAD");
 
-		tc.set("k","[5,4,3,2,1]");
+    tc.set("k","[5,4,3,2,1]");
 
-		tc.b=true;
-		tc.readString("b=false");
-		tc.readString("b=true");
-		tc.readString("b=f");
-		tc.readString("b=TrUE");
-		tc.readString("b=0");
+    tc.b=true;
+    tc.readString("b=false");
+    tc.readString("b=true");
+    tc.readString("b=f");
+    tc.readString("b=TrUE");
+    tc.readString("b=0");
 
-		tc.readFile("file.txt");
+    tc.readFile("file.txt");
 
-		tc.intSet.insert(100);
-		tc.intSet.insert(200);
+    tc.intSet.insert(100);
+    tc.intSet.insert(200);
 
-		tc.map["hello"] = 789;
-		tc.map["goodbye"] = 987;
+    tc.map["hello"] = 789;
+    tc.map["goodbye"] = 987;
 
-		tc.pair = make_pair(123, "test test");
-		tc.pair2 = make_pair("test2 test", 1.1f);
+    tc.pair = make_pair(123, "test test");
+    tc.pair2 = make_pair("test2 test", 1.1f);
 
-		tc.writeToStream(cout);
+    tc.writeToStream(cout);
 
-		string str = tc.toString();
-		tc2.readString(str);
-		if(tc!=tc2) throw runtime_error("Error: tc!=tc2");
+    string str = tc.toString();
+    tc2.readString(str);
+    if(tc!=tc2) throw runtime_error("Error: tc!=tc2");
 
-		static const int STRSIZE = 1000;
-		char str2[STRSIZE];
-		size_t size = tc.writeToString(str2,STRSIZE);
-		tc3.readString(str2, size);
-		if(tc!=tc3) throw runtime_error("Error: tc!=tc3");
+    static const int STRSIZE = 1000;
+    char str2[STRSIZE];
+    size_t size = tc.writeToString(str2,STRSIZE);
+    tc3.readString(str2, size);
+    if(tc!=tc3) throw runtime_error("Error: tc!=tc3");
 
-	}catch(exception& e){
-		cout<<e.what()<<endl;
-	}
+  }catch(exception& e){
+    cout<<e.what()<<endl;
+  }
 
-	#ifndef __GNUC__ 
-	cout << "Press enter to exit.\n";
-	getchar();
-	#endif
+  #ifndef __GNUC__ 
+  cout << "Press enter to exit.\n";
+  getchar();
+  #endif
 }
--- Test/TestConfig.h
+++ Test/TestConfig.h
@@ -3,58 +3,58 @@
 #include "../Configurator/configurator.h"
 
 struct SubConfig2:public codepi::Configurator{
-	int k;
+  int k;
 
-	CFG_HEADER(SubConfig2)
-	CFG_ENTRY2(k,9)
-	CFG_TAIL
+  CFG_HEADER(SubConfig2)
+  CFG_ENTRY2(k,9)
+  CFG_TAIL
 };
 
 struct SubConfig1:public SubConfig2{
-	int i,j;
-	
-	CFG_HEADER(SubConfig1)
-	CFG_ENTRY2(i,7)
-	CFG_ENTRY1(j)
-	CFG_PARENT(SubConfig2)
-	CFG_TAIL
+  int i,j;
+  
+  CFG_HEADER(SubConfig1)
+  CFG_ENTRY2(i,7)
+  CFG_ENTRY1(j)
+  CFG_PARENT(SubConfig2)
+  CFG_TAIL
 };
 
 struct TestConfig:public codepi::Configurator{
 
-	int jjj;
-	std::vector<int> k;
-	std::array<int,10> arr;
-	std::set<int> intSet;
-	std::pair<int, std::string> pair;
-	std::pair<std::string, float> pair2;
-	std::map<std::string, int> map;
-	std::string n;
-	SubConfig1 s;
-	std::vector<SubConfig1> t;
-	std::vector<std::string> strList;
-	SubConfig1 u;
-	bool b;
-	codepi::Optional<int> opt1, opt2, opt3;
-	codepi::Optional< std::vector<int> > optvec;
+  int jjj;
+  std::vector<int> k;
+  std::array<int,10> arr;
+  std::set<int> intSet;
+  std::pair<int, std::string> pair;
+  std::pair<std::string, float> pair2;
+  std::map<std::string, int> map;
+  std::string n;
+  SubConfig1 s;
+  std::vector<SubConfig1> t;
+  std::vector<std::string> strList;
+  SubConfig1 u;
+  bool b;
+  codepi::Optional<int> opt1, opt2, opt3;
+  codepi::Optional< std::vector<int> > optvec;
 
-	CFG_HEADER(TestConfig)
-	CFG_ENTRY2(jjj,12)
-	CFG_ENTRY1(k)
-	CFG_ENTRY1(arr)
-	CFG_ENTRY1(intSet)
-	CFG_ENTRY1(pair)
-	CFG_ENTRY1(pair2)
-	CFG_ENTRY1(map)
-	CFG_ENTRY2(n,"hello")
-	CFG_ENTRY1(s)
-	CFG_ENTRY1(t)
-	CFG_ENTRY1(strList)
-	CFG_ENTRY1(u)
-	CFG_ENTRY1(b)
-	CFG_ENTRY1(opt1)
-	CFG_ENTRY1(opt2)
-	CFG_ENTRY1(opt3)
-	CFG_ENTRY1(optvec)
-	CFG_TAIL
+  CFG_HEADER(TestConfig)
+  CFG_ENTRY2(jjj,12)
+  CFG_ENTRY1(k)
+  CFG_ENTRY1(arr)
+  CFG_ENTRY1(intSet)
+  CFG_ENTRY1(pair)
+  CFG_ENTRY1(pair2)
+  CFG_ENTRY1(map)
+  CFG_ENTRY2(n,"hello")
+  CFG_ENTRY1(s)
+  CFG_ENTRY1(t)
+  CFG_ENTRY1(strList)
+  CFG_ENTRY1(u)
+  CFG_ENTRY1(b)
+  CFG_ENTRY1(opt1)
+  CFG_ENTRY1(opt2)
+  CFG_ENTRY1(opt3)
+  CFG_ENTRY1(optvec)
+  CFG_TAIL
 };
