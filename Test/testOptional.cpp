f3771c7247cf6379983ea598628057e83010e6e5
pilardi <CodePi@users.noreply.github.com>
2016-11-14 01:18:34 -0500

    got rig of compile warnings

--- Test/testOptional.cpp
+++ Test/testOptional.cpp
@@ -24,49 +24,49 @@ int main(){
   int* orig = v.data();
   Optional<vector<int>> ov(move(v));
   int* curr = ov->data();
-  printf("construct by move contents:\t%x\t%x\t%s\n", orig, curr, pf(orig==curr));
+  printf("construct by move contents:\t%p\t%p\t%s\n", orig, curr, pf(orig==curr));
 
   // test2
   Optional<vector<int>> ov2(rvalue(&orig));
   curr = ov2->data();
-  printf("construct by rvalue:\t\t%x\t%x\t%s\n", orig, curr, pf(orig==curr));
+  printf("construct by rvalue:\t\t%p\t%p\t%s\n", orig, curr, pf(orig==curr));
 
   // test3
   vector<int> v3={1,2,3};
   orig = v3.data();
   Optional<vector<int>> ov3(v3);
   curr = ov3->data();
-  printf("construct by copy:\t\t%x\t%x\t%s\n", orig, curr, pf(orig!=curr));
+  printf("construct by copy:\t\t%p\t%p\t%s\n", orig, curr, pf(orig!=curr));
   
   // test4
   orig = ov3->data();
   Optional<vector<int>> ov4(ov3);
   curr = ov4->data();
-  printf("construct by copy wrapper:\t%x\t%x\t%s\n", orig, curr, pf(orig!=curr));
+  printf("construct by copy wrapper:\t%p\t%p\t%s\n", orig, curr, pf(orig!=curr));
 
   // test5
   Optional<vector<int>> ov5(Optional<vector<int>>(rvalue(&orig)));
   curr = ov5->data();
-  printf("construct by double rvalue:\t%x\t%x\t%s\n", orig, curr, pf(orig==curr));
+  printf("construct by double rvalue:\t%p\t%p\t%s\n", orig, curr, pf(orig==curr));
   
   // test6
   Optional<vector<int>> ov6;
   ov6 = rvalue(&orig);
   curr = ov6->data();
-  printf("assignment rvalue:\t\t%x\t%x\t%s\n", orig, curr, pf(orig==curr));
+  printf("assignment rvalue:\t\t%p\t%p\t%s\n", orig, curr, pf(orig==curr));
 
   // test7 
   Optional<vector<int>> ov7;
   ov7 = Optional<vector<int>>(rvalue(&orig));
   curr = ov7->data();
-  printf("assignment double rvalue:\t%x\t%x\t%s\n", orig, curr, pf(orig==curr));
+  printf("assignment double rvalue:\t%p\t%p\t%s\n", orig, curr, pf(orig==curr));
   
   // test8
   const vector<int> v8 = {1,2,3};
   orig = (int*)v8.data();
   Optional<vector<int>> ov8(v8);
   curr = ov8->data();
-  printf("construct by const:\t\t%x\t%x\t%s\n", orig, curr, pf(orig!=curr));
+  printf("construct by const:\t\t%p\t%p\t%s\n", orig, curr, pf(orig!=curr));
 
   // test9 
   vector<int> v9 = {1,2,3};
@@ -74,7 +74,7 @@ int main(){
   orig = v9.data();
   ov9 = v9;
   curr = ov9->data();
-  printf("assignment by copy:\t\t%x\t%x\t%s\n", orig, curr, pf(orig!=curr));
+  printf("assignment by copy:\t\t%p\t%p\t%s\n", orig, curr, pf(orig!=curr));
   
   // test10 
   Optional<vector<int>> v10(vector<int>({1,2,3}));
@@ -82,6 +82,6 @@ int main(){
   orig = v10->data();
   ov10 = v10;
   curr = ov10->data();
-  printf("assignment by copy wrapper:\t%x\t%x\t%s\n", orig, curr, pf(orig!=curr));
+  printf("assignment by copy wrapper:\t%p\t%p\t%s\n", orig, curr, pf(orig!=curr));
 
 }
