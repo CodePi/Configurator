22e5cdfcde44a754219abe7db5ab8adef3948ecc
CodePi <CodePi@users.noreply.github.com>
2017-02-09 00:31:51 -0500

    added test script

---
+++ tests/runtests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo --------------------------
+echo TestConfig
+./TestConfig
+echo --------------------------
+echo TestConfig2
+./TestConfig2
+echo --------------------------
+echo TestConfig3
+./TestConfig3
+echo --------------------------
+echo testOptional
+./testOptional
